06976cd1aaeb89198bf4293d06c335931bb3cd19
gsingh737 <ssgurpreetsingh@gmail.com>
2016-12-26 23:17:22 -0500

    Add DELETE /users/me route

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -4,7 +4,6 @@ var authenticate = (req, res, next) => {
   var token = req.header('x-auth');
   User.findByToken(token).then((user) => {
     if(!user){
-      console.log('Testing wrong path');
         return Promise.reject();
     }
     req.user = user;
--- server/models/user.js
+++ server/models/user.js
@@ -56,6 +56,18 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function(token){
+  var user = this;
+  return user.update({
+      $pull: {
+        tokens: {
+          token: token
+        }
+      }
+  });
+
+}
+
 UserSchema.statics.findbyCredential = function (email, password) {
   var User = this;
   return User.findOne({email}).then((user) => {
